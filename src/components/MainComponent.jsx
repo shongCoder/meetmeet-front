4bab2460c0e8a435dbf03601673e849f6172de88
PARK SO YOUNG <itnj.parksy@gmail.com>
2025-02-27 19:02:40 +0900

    메인 태블릿 사이즈 작업중

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,16 +2,16 @@ const MainComponent = () => {
   return (
     <>
       {/* main intro */}
-      <div className="relative bg-meet_black text-meet_white lg:w-full lg:h-[60rem] h-[50.75rem] flex justify-start bg-[url('/img/main/main_visual.png')] bg-cover bg-center">
-        <div className="w-full pt-80 bg-gradient-to-b from-gray-950/100 to-transparent">
+      <div className="relative bg-meet_black text-meet_white lg:w-full lg:h-[60rem] md:h-[64rem] h-[50.75rem] flex justify-start bg-[url('/img/main/main_visual.png')] bg-cover bg-center">
+        <div className="w-full lg:pt-80 md:pt-[23rem] bg-gradient-to-b from-gray-950/100 to-transparent">
           <div className="lg:px-[6.25rem] md:px-8 px-5 text-left">
-            <div className="lg:text-[3.25rem] text-[1.5rem] font-bold lg:leading-[4.25rem] leading-9">
+            <div className="lg:text-[3.25rem] md:text-[3.25rem] text-[1.5rem] font-bold lg:leading-[4.25rem] md:leading-[4.25rem] leading-9">
               모두가 주인공이 되는 공간 <br /> 소통하고, 공유하고, 소유하다.
             </div>
-            <div className="mt-4 lg:text-[1rem] text-[0.875rem] text-meet_gray">
+            <div className="mt-4 lg:text-[1rem] md:text-5 text-[0.875rem] text-meet_gray">
               만나고 싶은 순간, 지금 바로 밋밋하세요!
             </div>
-            <button className="lg:mt-10 mt-5 lg:px-8 lg:py-5 px-6 py-3 bg-meet_pink shadow-[inset_4px_4px_14px_0_rgba(255,255,255,0.2)] font-bold text-white rounded-full">
+            <button className="lg:mt-10 md:mt-10 mt-5 lg:px-8 lg:py-5 md:px-8 md:py-5 px-6 py-3 bg-meet_pink shadow-[inset_4px_4px_14px_0_rgba(255,255,255,0.2)] font-bold text-white rounded-full">
               앱 다운로드
             </button>
           </div>
@@ -20,13 +20,13 @@ const MainComponent = () => {
 
       {/* 실시간 영상통화 */}
       <div className="pt-32">
-        <div className="lg:w-36 w-20">
+        <div className="lg:w-36 md:w-[8.75rem] w-20">
           <img src="./img/main/AO.png" />
         </div>
-        <div className="text-meet_black text-center font-bold lg:text-[3.25rem] text-[1.75rem]">
+        <div className="text-meet_black text-center font-bold lg:text-[3.25rem] md:text-[3.25rem] text-[1.75rem]">
           실시간 영상통화
         </div>
-        <div className="text-meet_text lg:text-[1.25rem] text-[0.875rem] mt-4 mb-8 lg:w-[26rem] w-[12.875rem]">
+        <div className="text-meet_text lg:text-[1.25rem] md:text-[1.25rem] text-[0.875rem] mt-4 mb-8 pl-3 lg:w-[26rem] md:w-[24rem] w-[12.875rem]">
           지금 바로 연결하세요, 실시간으로 이루어지는 생생한 영상통화로 진짜
           소통을 경험해보세요.
         </div>
