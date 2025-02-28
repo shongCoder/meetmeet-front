e1ba07ee5e5b7f6c5eb5b9db44c7df78b26a2dd0
shongCoder <soyeong163@gmail.com>
2025-03-01 01:20:35 +0900

    앱 다운로드 페이지 웹, 태블릿, 모바일 적용

--- src/components/DownloadComponent.jsx
+++ src/components/DownloadComponent.jsx
@@ -1,20 +1,51 @@
 function DownloadComponent() {
   return (
     <div>
-      <div className="h-[60rem] bg-[url('/img/download/download_visual.png')] bg-cover bg-center lg:px-[6.25rem] flex">
-        <div className="w-[50%] h-[60rem] flex items-center">
+      {/*웹 버전*/}
+      <div className="h-[60rem] bg-[url('/img/download/download_visual.png')] bg-cover lg:bg-center md:bg-right md:bg-[-20rem] px-[6.25rem] flex lg:block md:block hidden">
+        <div className="lg:w-[50%] md:w-full h-[60rem] flex items-center lg:bg-meet_black/0 md:to-transparent md:bg-meet_black/80">
           <div>
             <img src="./img/download/app_icon.png" className="w-[16.375rem]" />
             <p className="text-5 text-meet_gray mt-6 text-center">
               만나고 싶은 순간, 지금 바로 밋밋하세요!
             </p>
-            <img
-              src="./img/download/store_button.png"
-              className="w-[26.3125rem] mt-10"
-            />
+            <div className="mt-10 flex gap-4">
+              <img
+                  src="./img/download/appstore.png"
+                  className="lg:w-[12.8125rem] md:w-[12.625rem]"
+              />
+              <img
+                  src="./img/download/playstore.png"
+                  className="lg:w-[12.8125rem] md:w-[12.625rem]"
+              />
+            </div>
+
           </div>
         </div>
-        <div className="w-[50%] h-[60rem]"></div>
+        <div className="w-[50%] h-[60rem] lg:block md:hidden"></div>
+      </div>
+
+      {/*모바일 버전*/}
+      <div className="w-full h-[50.75rem] bg-[url('/img/main/main_visual.png')] bg-cover lg:bg-center flex lg:hidden md:hidden blcok">
+        <div className="w-full h-[50.75em] px-5 flex items-center bg-gradient-to-b from-meet_black/100 via-[#5c001b]/95 to-meet_pink/95">
+          <div>
+            <img src="./img/download/app_icon.png" className="w-[13rem]" />
+            <p className="text-5 text-meet_gray mt-6 text-center">
+              만나고 싶은 순간, 지금 바로 밋밋하세요!
+            </p>
+            <div className="mt-10 flex gap-4">
+              <img
+                  src="./img/download/appstore.png"
+                  className="w-[10rem]"
+              />
+              <img
+                  src="./img/download/playstore.png"
+                  className="w-[10rem]"
+              />
+            </div>
+
+          </div>
+        </div>
       </div>
     </div>
   );
