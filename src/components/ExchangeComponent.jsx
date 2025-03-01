99f58f0a6418700fbb2f829abadec2091637d22e
shongCoder <soyeong163@gmail.com>
2025-03-01 17:12:35 +0900

    포인트 환전 페이지 완료

--- src/components/ExchangeComponent.jsx
+++ src/components/ExchangeComponent.jsx
@@ -2,15 +2,15 @@
 function ExchangeComponent() {
     return (
         <div>
-            <div className="mt-[10.625rem] text-meet_black text-center font-bold lg:text-[3.25rem] md:text-[3.25rem] text-[1.75rem]">
+            <div className="lg:mt-[10.625rem] md:mt-[10.625rem] mt-[8.75rem] text-meet_black text-center font-bold lg:text-[3.25rem] md:text-[3.25rem] text-[1.75rem]">
                 포인트 환전
             </div>
             <div className="text-meet_text text-center lg:text-[1.25rem] md:text-[1.25rem] text-[0.875rem] mt-2 mb-8 lg:w-[26rem] md:w-[24rem] w-[12.875rem]">
                 인플루언서 활동 수익을 환전할 수 있어요
             </div>
 
-            <div className="px-[13.75rem] w-full flex gap-5">
-                <div className="w-full border-meet_gray border p-5 flex items-center justify-between rounded-[0.75rem] shadow-[0px_12px_12px_rgba(54,49,189,0.02)]">
+            <div className="lg:px-[13.75rem] md:px-8 px-5 w-full lg:flex lg:gap-5 md:flex md:gap-5 flex-none">
+                <div className="w-full border-meet_gray border p-5 flex items-center justify-between rounded-[0.75rem] shadow-[0px_12px_12px_rgba(54,49,189,0.02)] lg:mb-0 md:mb-0 mb-2">
                     <span className="text-left font-medium text-meet_text text-[0.875rem] flex-1">보유중인 환전 가능 포인트</span>
                     <span className="text-right font-bold text-meet_pink text-[1.25rem] flex-1">600,000P</span>
                 </div>
@@ -25,7 +25,7 @@ function ExchangeComponent() {
             </div>
 
             {/* 수익금 내역 */}
-            <div className="px-[13.75rem] w-full mt-10">
+            <div className="lg:px-[13.75rem] md:px-8 px-5 w-full mt-10">
                 <span className="text-left text-[1.25rem] font-bold">수익금 내역</span>
                 <div className="w-full bg-meet_gray h-[1px] mt-4"></div>
                 <div className="w-full text-center mt-[3.75rem]">
@@ -36,19 +36,19 @@ function ExchangeComponent() {
             </div>
 
             {/* 수익금 환전 안내 */}
-            <div className="px-[13.75rem] w-full mt-20 mb-20">
+            <div className="lg:px-[13.75rem] md:px-8 px-5 w-full mt-20 mb-20">
                 <div className="bg-meet_light_gray rounded-[0.75rem] p-5">
                     <div className="font-bold text-meet_sodomy">수익금 환전 안내</div>
                     <div className="text-meet_text mt-2">
-                        <ul className="pl-2 space-y-2">
+                        <ul className="pl-2 space-y-2 text-[0.8125rem]">
                             <li className="relative flex items-start gap-2">
-                                <span className="text-meet_text text-[0.8125rem] leading-none translate-y-[4px]">•</span>
+                                <span className="text-meet_text text-sm leading-none translate-y-[4px]">•</span>
                                 <p className="flex-1">
                                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                 </p>
                             </li>
                             <li className="relative flex items-start gap-2">
-                                <span className="text-meet_text text-[0.8125rem] leading-none translate-y-[4px]">•</span>
+                                <span className="text-meet_text text-sm leading-none translate-y-[4px]">•</span>
                                 <p className="flex-1">
                                     It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                 </p>
