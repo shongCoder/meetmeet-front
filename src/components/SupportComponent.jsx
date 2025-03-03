b0f2678e0199719d4c9f661cb17408d6e6641e84
shongCoder <soyeong163@gmail.com>
2025-03-03 22:38:34 +0900

    고객지원 페이지 완료

--- src/components/SupportComponent.jsx
+++ src/components/SupportComponent.jsx
@@ -11,19 +11,10 @@ function SupportComponent() {
 
   return (
     <>
-      <div className="relative bg-meet_black text-meet_white lg:w-full lg:h-[56.125rem] md:h-[56.125rem] h-[56.125rem] flex justify-start">
-        {/* 이미지 (우측 정렬 & 아래로 8.125rem 튀어나오게 설정) */}
-        <img
-          src="./img/support/support_visual.png"
-          className="absolute right-0 top-0 h-[calc(100%+8.125rem)] w-[37.25rem]"
-        />
-
-        {/* 그라데이션 (이미지 위에 덮기) */}
-        <div className="absolute inset-0 bg-gradient-to-b from-gray-950/100 to-transparent z-10"></div>
-
+      <div className="relative w-full bg-meet_black text-meet_white lg:w-full lg:h-[56.125rem] md:h-[50.375rem] h-[50.75rem] flex flex-col items-center lg:px-[6.25rem] md:px-8 px-5">
         {/* 텍스트 & 버튼 (그라데이션 위에 위치) */}
-        <div className="relative z-20 w-full lg:pt-80 md:pt-[23rem] pt-[17.25rem]">
-          <div className="lg:px-[6.25rem] md:px-8 px-5 text-left">
+        <div className="relative z-20 w-full lg:pt-80 md:pt-[16.1875rem] pt-[10rem]">
+          <div className="text-left">
             <div className="lg:text-[3.25rem] md:text-[3.25rem] text-[1.5rem] font-bold lg:leading-[4.25rem] md:leading-[4.25rem] leading-9">
               모두가 주인공이 되는 공간 <br /> 소통하고, 공유하고, 소유하다.
             </div>
@@ -35,17 +26,33 @@ function SupportComponent() {
             </button>
           </div>
         </div>
+
+        {/* 이미지 (웹/모바일 분기) */}
+        <img
+          src="./img/support/support_visual.png"
+          className="absolute lg:right-0 lg:top-[-16rem] md:top-[-20rem] md:right-0 overflow-hidden
+               lg:w-[37.25rem] md:w-[37.25rem] w-full hidden md:block overflow-hidden"
+        />
+        <div className="md:hidden mt-10 absolute bottom-0 px-5">
+          <img
+            src="./img/support/support_visual_mobile.png"
+            className="h-auto object-cover"
+          />
+        </div>
+
+        {/* 배경 그라데이션 (웹 & 태블릿: 기본 그라데이션, 모바일: 하단에서 시작) */}
+        <div className="absolute inset-0 lg:bg-gradient-to-b md:bg-gradient-to-b w-full from-black/100 to-transparent bg-gradient-to-t md:bg-none z-10"></div>
       </div>
 
       {/* 이용 안내 */}
-      <div className="pt-[12.5rem] md:px-8">
-        <div className="bg-meet_pink text-meet_white text-[0.8125rem] font-semibold w-fit px-4 py-[0.375rem] rounded-2xl">
+      <div className="lg:px-[6.25rem] md:px-8 px-5 lg:mt-[18.75rem] md:mt-[18.75rem] pt-[5rem]">
+        <div className="bg-meet_pink text-meet_white lg:text-[0.8125rem] md:text-[0.8125rem] text-[0.625rem] font-semibold w-fit px-4 py-[0.375rem] rounded-2xl">
           Usage Information
         </div>
         <div className="text-meet_black text-center font-bold lg:text-[3.25rem] md:text-[3.25rem] text-[1.75rem] mb-2">
           이용 안내
         </div>
-        <div className="lg:px-[13.75rem] md:px-8 px-5 w-full">
+        <div className="lg:px-[13.75rem] w-full">
           {/* 아코디언 리스트 */}
           {[
             {
@@ -95,9 +102,13 @@ function SupportComponent() {
             </div>
           ))}
         </div>
+        <div className="text-meet_text text-center mt-9 lg:mb-[6.25rem] md:mb-[6.25rem]">
+          <span>더 궁금하신 내용이 있나요?</span>
+          <span className="font-medium text-meet_sodomy">문의하기</span>
+        </div>
       </div>
-      <div className="bg-meet_light_gray pt-[12.5rem] px-[6.25rem] w-full mt-[6.25rem] pb-[12.5rem]">
-        <div className="bg-meet_pink text-meet_white text-[0.8125rem] font-semibold w-fit px-4 py-[0.375rem] rounded-2xl">
+      <div className="bg-meet_light_gray lg:pt-[12.5rem] md:pt-[12.5rem] pt-[3.75rem] lg:px-[6.25rem] md:px-8 px-5 w-full lg:mt-[6.25rem] md:mt-[6.25rem] mt-[3.75rem] lg:pb-[12.5rem] md:pb-[12.5rem] pb-[3.75rem]">
+        <div className="bg-meet_pink text-meet_white lg:text-[0.8125rem] md:text-[0.8125rem] text-[0.625rem] font-semibold w-fit px-4 py-[0.375rem] rounded-2xl">
           News
         </div>
         <div className="text-meet_black text-center font-bold lg:text-[3.25rem] md:text-[3.25rem] text-[1.75rem] mb-2">
@@ -107,14 +118,14 @@ function SupportComponent() {
           밋밋의 새로운 소식을 알려드릴게요
         </div>
         {/* 게시물 박스 */}
-        <div className="mt-20 flex items-center justify-between gap-6">
+        <div className="mt-20 flex lg:items-center lg:justify-between gap-6 md:flex-col flex-col lg:flex-row">
           <div className="bg-meet_white p-6 w-full rounded-[1.25rem] border border-meet_gray">
             <div className="w-full h-[13.8rem] bg-[url('/img/support/blog-1.png')] bg-cover bg-center rounded-[0.75rem]"></div>
             <div className="w-full">
-              <div className="text-[1.5rem] font-bold mt-5">
+              <div className="lg:text-[1.5rem] md:text-[1.5rem] text-[1.25rem] text-ellipsis line-clamp-1 font-bold mt-5">
                 사회적 기업의 가치와 사회적 영향력
               </div>
-              <p className="text-meet_text mt-3 w-full text-ellipsis line-clamp-2">
+              <p className="text-meet_text mt-3 lg:text-[1rem] md:text-[1rem] text-[0.8125rem] w-full text-ellipsis lg:line-clamp-2 md:line-clamp-2 line-clamp-3">
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the standard dummy text ever
                 since the 1500s, when an unknown printer took a galley of type
@@ -129,13 +140,13 @@ function SupportComponent() {
             </div>
           </div>
           <div className="w-full flex flex-col gap-5 h-full">
-            <div className="w-full p-6 bg-meet_white flex items-center justify-between gap-[1.875rem] rounded-[1.25rem] border border-meet_gray">
-              <div className="w-60 h-[8.75rem] bg-[url('/img/support/blog-2.png')] bg-cover bg-center rounded-[0.75rem]"></div>
-              <div className="w-[19.25rem]">
-                <div className="text-[1.5rem] w-full text-ellipsis line-clamp-1 font-bold">
+            <div className="w-full p-6 bg-meet_white flex lg:items-center lg:justify-between md:items-center me:justify-between lg:flex-row md:flex-row flex-col gap-[1.875rem] rounded-[1.25rem] border border-meet_gray">
+              <div className="lg:w-60 md:w-60 w-full lg:h-[8.8125rem] md:h-[8.8125rem] h-[13.8rem] bg-[url('/img/support/blog-2.png')] bg-cover bg-center rounded-[0.75rem] m-0"></div>
+              <div className="lg:w-[19.25rem] md:w-[24.125rem] m-0">
+                <div className="lg:text-[1.5rem] md:text-[1.5rem] text-[1.25rem] w-full text-ellipsis line-clamp-1 font-bold">
                   자기 계발의 시작:5가지 효과적인 방법
                 </div>
-                <p className="text-meet_text mt-3 w-full text-ellipsis line-clamp-2">
+                <p className="text-meet_text mt-3 w-full text-ellipsis lg:line-clamp-2 md:line-clamp-2 line-clamp-3">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the standard dummy
                   text ever since the 1500s, when an unknown printer took a
@@ -145,13 +156,13 @@ function SupportComponent() {
                 <div className="font-normal text-meet_pink pt-8">read more</div>
               </div>
             </div>
-            <div className="w-full p-6 bg-meet_white flex items-center justify-between gap-[1.875rem] rounded-[1.25rem] border border-meet_gray">
-              <div className="w-60 h-[8.75rem] bg-[url('/img/support/blog-3.png')] bg-cover bg-center rounded-[0.75rem]"></div>
-              <div className="w-[19.25rem]">
-                <div className="text-[1.5rem] w-full text-ellipsis line-clamp-1 font-bold">
+            <div className="w-full p-6 bg-meet_white flex lg:items-center lg:justify-between md:items-center me:justify-between lg:flex-row md:flex-row flex-col gap-[1.875rem] rounded-[1.25rem] border border-meet_gray">
+              <div className="lg:w-60 md:w-60 w-full lg:h-[8.8125rem] md:h-[8.8125rem] h-[13.8rem] bg-[url('/img/support/blog-3.png')] bg-cover bg-center rounded-[0.75rem] m-0"></div>
+              <div className="lg:w-[19.25rem] md:w-[24.125rem] m-0">
+                <div className="lg:text-[1.5rem] md:text-[1.5rem] text-[1.25rem] w-full text-ellipsis line-clamp-1 font-bold">
                   창의력을 끌어올리는 방법:아이들에게 좋은 놀이
                 </div>
-                <p className="text-meet_text mt-3 w-full text-ellipsis line-clamp-2">
+                <p className="text-meet_text mt-3 w-full text-ellipsis lg:line-clamp-2 md:line-clamp-2 line-clamp-3">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has been the standard dummy
                   text ever since the 1500s, when an unknown printer took a
@@ -165,9 +176,12 @@ function SupportComponent() {
         </div>
       </div>
 
-      <div className="pt-[12.5rem] px-[6.25rem] w-full ">
+      <div className="lg:pt-[12.5rem] md:pt-[12.5rem] pt-[3.75rem] lg:px-[6.25rem] md:px-8 px-5 w-full ">
         <div className="">
-          <img src="./img/support/AO.png" className="w-[6.25rem]" />
+          <img
+            src="./img/support/AO.png"
+            className="lg:w-[6.25rem] md:w-[6.25rem] w-20"
+          />
         </div>
         <div className="text-meet_black text-center font-bold lg:text-[3.25rem] md:text-[3.25rem] text-[1.75rem] mb-2">
           자주 묻는 질문
@@ -175,7 +189,7 @@ function SupportComponent() {
         <div className="text-meet_text text-center lg:text-[1.25rem] md:text-[1.25rem] text-[0.875rem] mt-2 mb-8 w-full">
           Frequently Asked Questions
         </div>
-        <div className="lg:px-[13.75rem] md:px-8 px-5 w-full">
+        <div className="lg:px-[13.75rem] w-full">
           {/* 아코디언 리스트 */}
           {[
             {
@@ -225,7 +239,7 @@ function SupportComponent() {
             </div>
           ))}
         </div>
-        <div className="text-meet_text text-center mt-9 mb-[6.25rem]">
+        <div className="text-meet_text text-center mt-9 lg:mb-[6.25rem] md:mb-[6.25rem] mb-[3.75rem]">
           <span>더 궁금하신 내용이 있나요?</span>
           <span className="font-medium text-meet_sodomy">문의하기</span>
         </div>
